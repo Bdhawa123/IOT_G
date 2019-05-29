96bc00db2973e2e4fe24374af26b155ab88f7474
Bdhawa123 <42852141+Bdhawa123@users.noreply.github.com>
2019-05-29 20:31:14 +1000

    Update motion_sensor.ino

--- arduino/motion_sensor/motion_sensor.ino
+++ arduino/motion_sensor/motion_sensor.ino
@@ -11,36 +11,25 @@ void setup() {
   Serial.begin(9600);
 }
  
-void loop(){
-  
+void loop(){  
   val = digitalRead(inputPin);  // read input value
-
-  //Serial.println("test1");
   if (val == HIGH) {            // check if the input is HIGH
     digitalWrite(ledPin, HIGH);  // turn LED ON
-                      // wait for 5 sec
+                  
    
    
-
-    
     if (pirState == LOW) {
       // we have just turned on
-      //Serial.println("Motion detected!");
-      // We only want to print on the output change, not state
       pirState = HIGH;
       Serial.println(pirState);
-      
         val=LOW;
     }
    
   } else {
                   
-    digitalWrite(ledPin, LOW); // turn LED OFF
-     
+    digitalWrite(ledPin, LOW); // turn LED OFF    
     if (pirState == HIGH){
       // we have just turned of  
-       
-      // We only want to print on the output change, not state
       pirState = LOW;
       Serial.println(pirState);
       
